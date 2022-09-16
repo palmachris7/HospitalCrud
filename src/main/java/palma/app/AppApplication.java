7e01691f7b2130251dc8b9e7bca0334c0dc195c8
palmachris7 <palmachristopher7@hotmail.com>
2022-09-15 20:28:39 -0500

    Properties

--- src/main/java/palma/app/AppApplication.java
+++ src/main/java/palma/app/AppApplication.java
@@ -1,33 +1,14 @@
 package palma.app;
 
-import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 
-import palma.app.models.Gerente;
 
 @SpringBootApplication
-public class AppApplication implements CommandLineRunner {
+public class AppApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
-	@Autowired
-    private JdbcTemplate jdbcTemplate;
-	
-	@Override
-	public void run(String... args) throws Exception {
-		String sql = "SELECT * FROM GERENTE";
-         
-        List<Gerente> gerentes = jdbcTemplate.query(sql,BeanPropertyRowMapper.newInstance(Gerente.class));
-         System.out.println("Hola"+gerentes);
-        gerentes.forEach(System.out :: println);
-		
-	}
-
 }
